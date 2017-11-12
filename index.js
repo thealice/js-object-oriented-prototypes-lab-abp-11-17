9518de15070a4257316bb76e6b9a09b877d6d5cd
Alice Freda <alicefreda@gmail.com>
2017-11-12 19:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function() {
   return ("No, I must disagree");
 }
+BoardMember.prototype.approve = function() {
+  return ("You can do that!");
+}
 //
 //   this.veto = function(){
 //     return 'No, I must disagree'
